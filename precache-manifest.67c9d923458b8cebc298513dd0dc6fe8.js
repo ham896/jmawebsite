e0fdc11b92c5d4cc0657d8223ee3153c7ebbfb45
ham896 <ham896@mail.usask.ca>
2020-09-19 22:12:30 +0800

    Updates

---
+++ precache-manifest.67c9d923458b8cebc298513dd0dc6fe8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ff08cfc6f0c1b102e9106872cdbb5a",
+    "url": "/jmawebsite/index.html"
+  },
+  {
+    "revision": "42d267a4c145306e8c00",
+    "url": "/jmawebsite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f7506d485d96d436dd5",
+    "url": "/jmawebsite/static/js/2.4cbe87e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jmawebsite/static/js/2.4cbe87e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42d267a4c145306e8c00",
+    "url": "/jmawebsite/static/js/main.fc1ef67a.chunk.js"
+  },
+  {
+    "revision": "3954b141874dedfa1c96",
+    "url": "/jmawebsite/static/js/runtime-main.05e6b30c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jmawebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
